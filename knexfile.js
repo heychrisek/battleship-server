7dbc7b9a2e796c87916ea811254d9c70b4debdf9
chriskohlbrenner <c.kohlbrenner@gmail.com>
2016-10-16 16:12:28 -0400

    knex

--- knexfile.js
+++ knexfile.js
@@ -1,13 +1,8 @@
 module.exports = {
-  development: {
-    client: 'pg',
-    connection: { user: 'cek', database: 'battleship', host: 'localhost' }
-  },
-  production: {
-    client: 'postgresql',
-    connection: 'postgres://nkpwgmaxfjshhp:c6DIW4EziII-effGiHO7dFSjad@ec2-54-243-203-87.compute-1.amazonaws.com:5432/dadraqijfvt324?ssl=true',
-    migrations: {
-      tableName: 'migrations'
-    }
+  client: 'pg',
+  debug: true,
+  connection: 'postgres://nkpwgmaxfjshhp:c6DIW4EziII-effGiHO7dFSjad@ec2-54-243-203-87.compute-1.amazonaws.com:5432/dadraqijfvt324' + '?ssl=true'
+  migrations: {
+    tableName: 'migrations'
   }
-};
+}
