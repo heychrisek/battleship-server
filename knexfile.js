285eb11c0ce5d3daab271e16fe66ae15c7425535
chriskohlbrenner <c.kohlbrenner@gmail.com>
2016-10-16 16:06:12 -0400

    knex

--- knexfile.js
+++ knexfile.js
@@ -5,9 +5,17 @@ module.exports = {
   },
   production: {
     client: 'postgresql',
-    connection: 'postgres://nkpwgmaxfjshhp:c6DIW4EziII-effGiHO7dFSjad@ec2-54-243-203-87.compute-1.amazonaws.com:5432/dadraqijfvt324',
+    connection: {
+      host: process.env.PG_HOST || 'localhost',
+      user: process.env.PG_USER || 'postgres',
+      database: 'postgres://nkpwgmaxfjshhp:c6DIW4EziII-effGiHO7dFSjad@ec2-54-243-203-87.compute-1.amazonaws.com:5432/dadraqijfvt324',
+    },
+    pool: {
+        min: 2,
+        max: 10
+    },
     migrations: {
-      tableName: 'migrations'
+        tableName: 'migrations'
     }
   }
 };
